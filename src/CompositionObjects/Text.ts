ad9cb7b5d318c954c81521669ef8c7e78d181319
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-07-04 18:57:14 +0100

    feat(composition): add text class

---
+++ src/CompositionObjects/Text.ts
@@ -0,0 +1,60 @@
+export enum TextType {
+  plainText = "plain_text",
+  mrkdwn = "mrkdwn",
+}
+
+/** This class represents a Slack TextObject.
+ *  For more info kindly visit https://api.slack.com/reference/messaging/composition-objects#text
+ */
+export default class Text {
+  public type: TextType;
+  public text: string;
+  public emoji?: boolean;
+  public verbatim?: boolean;
+
+  /**
+   * @description Create a new text object
+   * @param  {TextType} type The type of text object to be created
+   * @param  {string} text The text string in the object
+   * @param  {boolean} emoji? Does the text contain emojis?
+   * @param  {boolean} verbatim? Should the text be rendered verbatim?
+   */
+  constructor(type: TextType, text: string, emoji?: boolean, verbatim?: boolean) {
+    if (emoji !== undefined) {
+      this.validateEmoji(type, emoji);
+      this.emoji = emoji;
+    }
+
+    if (verbatim !== undefined) {
+      this.validateVerbatim(type, verbatim);
+      this.verbatim = verbatim;
+    }
+
+    this.type = type;
+    this.text = text;
+  }
+
+  /**
+   * @description Ensure emoji is not being used with a plain text type.
+   * @param  {TextType} type The type of the text object
+   * @param  {boolean} emoji Wether or not emojis should be used
+   * @returns void
+   */
+  private validateEmoji(type: TextType, emoji: boolean): void {
+    if (type === TextType.mrkdwn && emoji === true) {
+      throw new Error('You cannot use a emojis with mrkdwn type.');
+    }
+  }
+
+  /**
+   * @description Ensure user cannot use verbatim with plain_text
+   * @param  {TextType} type The type of the text object
+   * @param  {boolean} verbatim Wether or not the text should be rendered verbatim
+   * @returns void
+   */
+  private validateVerbatim(type: TextType, verbatim: boolean): void {
+    if (type === TextType.plainText && verbatim === true) {
+      throw new Error('You cannot use verbatim with plain_text type.')
+    }
+  }
+}
---
+++ src/CompositionObjects/__tests__/Text.spec.ts
@@ -0,0 +1,50 @@
+import Text, { TextType } from '../Text';
+
+describe('Text', () => {
+  it('should create a new text object', () => {
+    const text = new Text(TextType.plainText, 'Some plain text');
+
+    expect(text).toEqual({
+      text: 'Some plain text',
+      type: 'plain_text',
+    });
+  })
+
+  it('should create a new text with emoji', () => {
+    const text = new Text(TextType.plainText, 'Some emoji text', true);
+
+    expect(text).toEqual({
+      emoji: true,
+      text: 'Some emoji text',
+      type: 'plain_text',
+    });
+  });
+
+  it('should create a new text with verbatim', () => {
+    const text = new Text(TextType.mrkdwn, 'Some verbatim text', undefined, true);
+
+    expect(text).toEqual({
+      text: 'Some verbatim text',
+      type: 'mrkdwn',
+      verbatim: true,
+    });
+  });
+
+  it('should throw an error if emoji is paired with mrkdwn', (done) => {
+    try {
+      const text = new Text(TextType.mrkdwn, 'Some emoji text', true);
+    } catch(error) {
+      expect(error.message).toEqual('You cannot use a emojis with mrkdwn type.');
+      done();
+    }
+  });
+
+  it('should throw an error if verbatim is paired with plain_text', (done) => {
+    try {
+      const text = new Text(TextType.plainText, 'Some verbatim text', undefined, true);
+    } catch(error) {
+      expect(error.message).toEqual('You cannot use verbatim with plain_text type.');
+      done();
+    }
+  });
+});
